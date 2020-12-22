f920ba6bed0df6a0168a72f3d85022096c2623ec
Eric Bouchut <ebouchut@gmail.com>
2020-12-22 19:25:29 +0100

    Add BetContract skeleton

---
+++ contracts/Bet.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+// TODO: Use the latest compiler version
+pragma solidity 0.6.11;
+
+contract Bet {
+
+    struct Event {
+        uint id;
+        string name;
+        uint eventDate;
+
+    }
+
+
+    function createEvent(string memory _name, uint _eventDate, uint  ) {
+
+    }
+
+}
