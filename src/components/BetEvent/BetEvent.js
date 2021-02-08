ee0e9329db7c76a56bf3977069101d2e207666be
tanteli <andriatanteli@gmail.com>
2021-02-08 11:04:20 +0100

    update project

---
+++ src/components/BetEvent/BetEvent.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function BetEvent() {
+  return (
+    <div className="card text-center">
+      <div className="card-header">Your bets</div>
+      <div className="card-body">
+        <p>Hello Bet</p>
+      </div>
+    </div>
+  );
+}
+
+export default BetEvent;
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,21 +1,27 @@
 import React, { useState, useContext, useEffect } from "react";
 import Web3Context from "../Web3context";
+import logo from "../../logo200x200.png";
 
 function Navbar() {
+  const style = {
+    padding: "10px",
+  };
 
-    const style = {
-        padding:"10px"
-    }
-    
-  const web3Context = useContext(Web3Context);  
+  const web3Context = useContext(Web3Context);
   const { web3, accounts, contract, currentAccount } = web3Context;
 
   return (
     <div>
       <nav className="navbar navbar-dark bg-dark shadow mb-5">
-        <p className="navbar-brand my-auto" style={style}>Bet-No-Loss</p>
+        <p className="navbar-brand my-auto" style={style}>
+          Bet-No-Loss&nbsp;
+          <img src={logo} style={{ width: "50px", marginBottom: "10px" }} />
+        </p>
         <ul className="navbar-nav">
-          <li className="nav-item text-white" style={style}> {currentAccount}</li>
+          <li className="nav-item text-white" style={style}>
+            {" "}
+            {currentAccount}
+          </li>
         </ul>
       </nav>
     </div>
