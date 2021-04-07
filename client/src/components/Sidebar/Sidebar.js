515664812a54e8ad4ca397f9d0339b6dbbddb724
Lebeil <liedel75@gmail.com>
2021-04-07 22:12:42 +0200

    Bootstrap + navbar & card

--- client/src/components/Sidebar/Sidebar.js
+++ client/src/components/Sidebar/Sidebar.js
@@ -2,14 +2,28 @@ import React from "react";
 
 function Sidebar() {
   return (
-    <aside className="menu column is-2 pt-4 ">
-      <p className="button is-rounded mb-4">
-        <a>Dashboard</a>
-      </p>
-      <p className="button is-rounded">
-        <a>Customers</a>
-      </p>
-    </aside>
+    <>
+      <div className="">
+        <p className="button is-rounded mb-4">
+          <a>Dashboard</a>
+        </p>
+        <p className="button is-rounded">
+          <a>Customers</a>
+        </p>
+      </div>
+      <div className="m-5">
+        <div
+          class="card bg-dark text-white container"
+          style={{ height: "160px" }}
+        >
+          <p class="card-text pt-5">
+            Il y a actuellement $162.05 million déposé et $217,896 de
+            récompenses hebdomadaire !
+          </p>
+          <p class="card-text">Last updated 3 mins ago</p>
+        </div>
+      </div>
+    </>
   );
 }
 
