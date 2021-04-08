16209c057f6742f3638f388e00413ac226dc1198
Lebeil <liedel75@gmail.com>
2021-04-08 18:35:42 +0200

    add cards hover shadow

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -5,28 +5,28 @@ function Card(bet) {
   /* const {hometeam, awayteam, date, type, img} = props; */
   return (
     <div className="container">
-      <div className="card mb-3 hovv">
+      <div className="card mb-3">
         <div class="row no-gutters">
           <div className="col-md-6 text-center">
             <div className="d-flex flex-column">
-              <a className="card-footer-item">{bet.hometeam}</a>
-              <div className="card-footer-item">
+              <a>{bet.hometeam}</a>
+              <div>
                 <img src={bet.img} width={100} />
               </div>
-              <p className="card-footer-item">{bet.awayteam}</p>
+              <p>{bet.awayteam}</p>
             </div>
           </div>
           <div className="col-md-6 text-center">
             <div className="d-flex flex-column">
-              <a href="#" className="card-footer-item">
+              <a href="#">
                 <time>{bet.date}</time>
               </a>
-              <a href="#" className="card-footer-item">
+              <a href="#">
                 <button type="button" class="btn btn-default">
                   Compte à rebours
                 </button>
               </a>
-              <a href="#" className="card-footer-item">
+              <a href="#">
                 <button
                   type="button"
                   className="btn btn-primary btn-lg"
