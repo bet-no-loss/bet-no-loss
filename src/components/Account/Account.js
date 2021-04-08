f0e4c49fa4849f39b69eedc0e1740be0ad7bcef0
Lebeil <liedel75@gmail.com>
2021-04-08 19:32:56 +0200

    add router

---
+++ src/components/Account/Account.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Account = () => {
+    return (
+        <div>
+            <h1>Votre compte </h1>
+        </div>
+    );
+};
+
+export default Account;
---
+++ src/components/Admin/Admin.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Admin = () => {
+    return (
+        <div>
+           <h1>Admin page</h1>
+        </div>
+    );
+};
+
+export default Admin;
