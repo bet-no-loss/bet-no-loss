66fa3d07b2d22ad6c3db24923737db46e78c3db6
tanteli <andriatanteli@gmail.com>
2021-04-20 23:39:27 +0200

    implement addSportEvent

--- contracts/Play.sol
+++ contracts/Play.sol
@@ -20,6 +20,27 @@ contract Play {
     constructor(address _tokenAddress) {
         Dai = IERC20(_tokenAddress);
     } 
+
+    function deposit(uint _amount, address _sender) public payable {
+        require(_amount >= 10, "Error, deposit must be >= 10 DAI");
+
+        Dai.transferFrom(_sender, address(this), _amount);
+    }
+
+    function addSportEvent(
+        string memory _name,
+        string memory _teamA,
+        string memory _teamB,
+        uint          _date
+    ) public returns (bool)
+    {
+        bytes memory bytesName = bytes(_name);
+        require(bytesName.length > 0, "_name cannot be empty");
+        require(_date >= block.timestamp + 1 weeks, "_date must be >= 1 week from now");
+
+        // Add the sport event
+        events.push(SportEvent(_name, _teamA, _teamB, _date, _teamA));
+    }
       
     
 }
