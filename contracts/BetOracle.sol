bf07a1cd5017c5fd26a7274c96f2a1b8f16975fe
Eric Bouchut <ebouchut@gmail.com>
2021-05-03 14:05:46 +0200

    Upgrade solidity compiler to 0.8.4
    Fix an important bug in the Solidity ABI decoder v2
    
    See: https://forum.soliditylang.org/t/solidity-v0-8-4-is-here-adds-custom-structured-errors-and-more/242
    
    See: https://blog.soliditylang.org/2021/04/21/solidity-0.8.4-release-announcement/

--- contracts/BetOracle.sol
+++ contracts/BetOracle.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.3;
+pragma solidity ^0.8.4;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
--- contracts/OracleInterface.sol
+++ contracts/OracleInterface.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.3;
+pragma solidity ^0.8.4;
 
 /**
  * @title The interface for the events Oracle (`BetOracle`)
